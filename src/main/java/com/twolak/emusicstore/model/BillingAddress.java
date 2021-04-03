e1f9eca97c01047e844eb8bbfee93d993a7cb74d
twolak88 <twolak88@gmail.com>
2021-04-03 11:26:48 +0200

    Clean imports

--- src/main/java/com/twolak/emusicstore/model/BillingAddress.java
+++ src/main/java/com/twolak/emusicstore/model/BillingAddress.java
@@ -12,7 +12,6 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
